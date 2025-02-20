af4ea85a5811100e9f30c90749d5ac258df18000
HassanZohaib121 <hassan.zohaib.184@gmail.com>
2025-02-21 01:17:55 +0500

    finished

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,15 +1,8 @@
-import { verifySession } from "@/lib/session";
-import { redirect } from "next/navigation";
-
 export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-    const session = await verifySession();
-    if(!session){
-        redirect("/login")
-    }
   return (
       <main className=" place-content-center">
         {children}
